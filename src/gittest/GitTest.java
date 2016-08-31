62a1049327c8db83396fedb9fb49eb68f7814a88
davidspagnolo <david.spagnolo@delta.com>
2016-08-31 11:29:18 -0500

    test dev method

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         System.out.println("Testing");
+        newDevelopmentMehtod();
     }
     
     public static void newDevelopmentMehtod(){
